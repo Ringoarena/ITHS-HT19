6cc87e79832e602c43402f340e748afeeb337183
Rikard Virta <rikard.virta90@gmail.com>
2019-12-03 13:51:31 +0100

    Cleaned JPAImplementation.getAll().

--- Databases Lab 4/src/model/JPAImplementation.java
+++ Databases Lab 4/src/model/JPAImplementation.java
@@ -24,11 +24,8 @@ public class JPAImplementation implements PersonDAO {
     @Override
     public List<Person> getAll() {
         EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        List<Person> list = em.createQuery("from Person", Person.class)
+        return em.createQuery("from Person", Person.class)
                 .getResultList();
-        em.getTransaction().commit();
-        return list;
     }
 
     @Override
